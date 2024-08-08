5b85e0ced47be547241da0777da5fcbd60848ef3
heejin <heejin0857@naver.com>
2024-08-09 02:16:31 +0900

    infinite scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: {
@@ -19,9 +16,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${inter.className} h-screen bg-neutral-900 text-white max-w-screen-sm mx-auto`}>
-                {children}
-            </body>
+            <body className="min-h-screen bg-neutral-900 text-white max-w-screen-sm mx-auto">{children}</body>
         </html>
     );
 }
