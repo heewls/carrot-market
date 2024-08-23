1842585e4a464f8566bd257ea6eb520f9c5583c4
heejin <heejin0857@naver.com>
2024-08-23 18:27:05 +0900

    add textarea

--- components/input.tsx
+++ components/input.tsx
@@ -1,18 +1,35 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, TextareaHTMLAttributes } from "react";
 
-interface InputProps {
+interface InputOrTextareaProps {
     errors?: string[];
     name: string;
+    height?: number;
+    textarea?: boolean;
 }
 
-export default function Input({ errors = [], name, ...rest }: InputProps & InputHTMLAttributes<HTMLInputElement>) {
+export default function Input({
+    errors = [],
+    name,
+    height = 40,
+    textarea = false,
+    ...rest
+}: InputOrTextareaProps & (InputHTMLAttributes<HTMLInputElement> | TextareaHTMLAttributes<HTMLTextAreaElement>)) {
     return (
         <div className="flex flex-col gap-2">
-            <input
-                className="bg-transparent rounded-md w-full h-10 border-none ring-1 ring-neutral-200 transition focus:outline-none focus:ring-4 focus:ring-orange-500 placeholder:text-neutral-400"
-                name={name}
-                {...rest}
-            />
+            {textarea ? (
+                <textarea
+                    className="bg-transparent rounded-md w-full min-h-10 border-none ring-1 ring-neutral-200 transition focus:outline-none focus:ring-4 focus:ring-orange-500 placeholder:text-neutral-400"
+                    name={name}
+                    style={{ height: `${height}px` }}
+                    {...(rest as TextareaHTMLAttributes<HTMLTextAreaElement>)}
+                />
+            ) : (
+                <input
+                    className="bg-transparent rounded-md w-full h-10 border-none ring-1 ring-neutral-200 transition focus:outline-none focus:ring-4 focus:ring-orange-500 placeholder:text-neutral-400"
+                    name={name}
+                    {...(rest as InputHTMLAttributes<HTMLInputElement>)}
+                />
+            )}
             {errors?.map((error, idx) => (
                 <span className="text-red-500 font-medium" key={idx}>
                     {error}
